578b5ab87d4cb9444caafa99555762b614ce5915
J <zigilkadu@gmail.com>
2017-12-20 15:43:24 +0100

    Prototype Upload
    upload des leicht anonymisierten prototypen

---
+++ files/createrecipe/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v],w,_(x,y,z,A,B,C,D,_(),E,_(F,G,H,I,J,_(K,L,M,N),O,null,P,I,Q,I,R,S,T,null,U,V,W,X,Y,Z,ba,V),bb,_(),bc,_(),bd,_(be,[])),bf,_(),bg,_());}; 
+var b="url",c="createrecipe.html",d="generationDate",e=new Date(1513780634488.38),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="SearchBoxText",o="FilterEventsFull",p="CustomEventName",q="CustomEventDescription",r="CustomEventLocation",s="CustomEventMaxMembers",t="CustomEventTime",u="BananaLasagnaPap",v="UserName",w="page",x="packageId",y="712f91f94b124b5498a74ae24a97fba5",z="type",A="Axure:Page",B="name",C="CreateRecipe",D="notes",E="style",F="baseStyle",G="627587b6038d43cca051c114ac41ad32",H="pageAlignment",I="near",J="fill",K="fillType",L="solid",M="color",N=0xFFFFFFFF,O="image",P="imageHorizontalAlignment",Q="imageVerticalAlignment",R="imageRepeat",S="auto",T="favicon",U="sketchFactor",V="0",W="colorStyle",X="appliedColor",Y="fontName",Z="Applied Font",ba="borderWidth",bb="adaptiveStyles",bc="interactionMap",bd="diagram",be="objects",bf="masters",bg="objectPaths";
+return _creator();
+})());
---
+++ files/myfriends/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v],w,_(x,y,z,A,B,C,D,_(),E,_(F,G,H,I,J,_(K,L,M,N),O,null,P,I,Q,I,R,S,T,null,U,V,W,X,Y,Z,ba,V),bb,_(),bc,_(),bd,_(be,[])),bf,_(),bg,_());}; 
+var b="url",c="myfriends.html",d="generationDate",e=new Date(1513780636115.68),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="SearchBoxText",o="FilterEventsFull",p="CustomEventName",q="CustomEventDescription",r="CustomEventLocation",s="CustomEventMaxMembers",t="CustomEventTime",u="BananaLasagnaPap",v="UserName",w="page",x="packageId",y="1bcb26abb3834d63bc7aa8edaf5fd27e",z="type",A="Axure:Page",B="name",C="MyFriends",D="notes",E="style",F="baseStyle",G="627587b6038d43cca051c114ac41ad32",H="pageAlignment",I="near",J="fill",K="fillType",L="solid",M="color",N=0xFFFFFFFF,O="image",P="imageHorizontalAlignment",Q="imageVerticalAlignment",R="imageRepeat",S="auto",T="favicon",U="sketchFactor",V="0",W="colorStyle",X="appliedColor",Y="fontName",Z="Applied Font",ba="borderWidth",bb="adaptiveStyles",bc="interactionMap",bd="diagram",be="objects",bf="masters",bg="objectPaths";
+return _creator();
+})());
